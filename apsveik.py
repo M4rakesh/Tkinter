f535b13d94dd850e0cd8a81eac6b70cc5e7319a1
M4rakesh <maksims.rogozins@gmail.com>
2024-12-19 09:35:09 +0200

    1

--- apsveik.py
+++ apsveik.py
@@ -28,28 +28,39 @@ def novelejums():
            'Lai Jaunais gads,\nkā teiksmu avots,Ir neizsmeļams,\nveselīgs un tīrs!']
     value=choice(novel)
     lableText.configure(text=value)
+def prog():
+    with open("nov.txt","r",encoding='utf-8')as f:
+        lines=f.readlines()
+        if lines:
+            random_line=choice(lines).strip()
+            label1.config(text=f'Prognoze uz 2025.g: {random_line}')
+            label1.place(x=360,y=575)
+            
+        else:
+            label1.config(text='Fails ir tuksš')
 e=Entry(root,show='*')
 e.pack()
 lableText=Label(root,
               text='',
-              font=('Comic Sans MS ',20,'bold'),
-              #bg='',
+              font=('Comic Sans MS ',15,'bold'),
               fg='black'
               )
 lableText.place(x=360,y=600)
 
 btn1=Button(root,font='Arial 15',text='Vārds',command=get,border=True,borderwidth=3,width=15,height=3)
 btn1.pack()
-btn1.place(x=50,y=75)
-
+btn1.place(x=50,y=100)
 btn2=Button(root,font='Arial 15',text='Krāsa',command=change_color,width=15,height=3,border=True,borderwidth=3)
 btn2.pack()
-btn2.place(x=50,y=225)
+btn2.place(x=50,y=200)
 btn3=Button(root,font='Arial 15',text='Novēlējums',command=novelejums,width=15,height=3,border=True,borderwidth=3)
 btn3.pack()
-btn3.place(x=50,y=375)
+btn3.place(x=50,y=300)
+btn4=Button(root,font='Arial 15',text='Prognoze',command=prog,width=15,height=3,border=True,borderwidth=3)
+btn4.pack()
+btn4.place(x=50,y=400)
 
-label1=Label(root,fg='black',font=('Arial 20 bold'))
+label1=Label(root,fg='black',font=('Arial 15 bold'))
 label1.pack()
 
 root.mainloop()
